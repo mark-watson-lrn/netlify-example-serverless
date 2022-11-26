8fc33473e3bac3b2b8f5030ad6af59ab9af5251d
Mark Watson <mark.watson@learnosity.com>
2022-11-26 22:01:35 +1100

    feature: add post requests to pokedex api

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -2,14 +2,19 @@
 // so you will get an error message saying you need to use import instead of
 // require
 const fetch = require('node-fetch');
-exports.handler = async function () {
-    const POKE_API='https://pokeapi.co/api/v2/pokedex/kanto';
 
+exports.handler = async function (event, context) {
+    const eventBody = JSON.parse(event.body)
+    const POKE_API='https://pokeapi.co/api/v2/pokedex/'+eventBody.region;
     const response = await fetch(POKE_API);
     const data = await response.json();
 
+    console.log( event );
+
     return {
         statusCode: 200,
-        body: JSON.stringify(data)
+        body: JSON.stringify({
+            pokemon: data.pokemon_entries
+        })
     }
 }
